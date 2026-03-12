e197a9c88287fb6c4efe117f609a6b8c601e3037
Silen Locatelli <silen.locatelli@gmx.ch>
2026-03-12 08:50:50 +0100

    refactor: dyn table

--- src/db.rs
+++ src/db.rs
@@ -13,3 +13,49 @@ pub fn connect(config: &config::Server) -> Ch {
 
     client
 }
+
+#[allow(unused)]
+pub async fn create_dyn_table(table: DynTable, ch: Ch) -> DynTable {
+    ch.query(&format!(
+        "CREATE TABLE IF NOT EXISTS {} ({}) ENGINE = MergeTree() ORDER BY tuple()",
+        table.name,
+        table.fields.join(", ")
+    ))
+    .execute()
+    .await
+    .unwrap();
+
+    // insert rows
+    if !table.rows.is_empty() {
+        let values_list: Vec<String> = table
+            .rows
+            .iter()
+            .map(|row| format!("({})", row.join(", ")))
+            .collect();
+
+        ch.query(&format!(
+            "INSERT INTO {} VALUES {}",
+            table.name,
+            values_list.join(", ")
+        ))
+        .execute()
+        .await
+        .unwrap();
+    }
+
+    table
+}
+
+#[allow(unused)]
+pub struct DynTable {
+    pub name: String,
+    pub fields: Vec<String>,
+    pub rows: Vec<Vec<String>>,
+}
+
+#[allow(unused)]
+impl DynTable {
+    pub fn new(name: String, fields: Vec<String>, rows: Vec<Vec<String>>) -> Self {
+        Self { name, fields, rows }
+    }
+}
